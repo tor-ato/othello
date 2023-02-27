b9465e02436d89b5a6d58d60e91160ac58c16fd3
torato <toratokitahara@gmail.com>
2023-02-27 20:46:55 +0900

    add cointos,only where you can put exsept place there is, count 0 and if 0 is 0 finish

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def display(board):
     # print(board, sep='\n')
     for i in board:
@@ -8,9 +11,7 @@ def input_rock(player):
     input_rock = list(map(int,input(f"{player}、石を置きたい場所を指定して下さい").split()))
     return input_rock
 
-def reversible_rock(board,X,Y,player):
-    sentinel = 0
-    go_strait_check = True
+def count_reversible_rock(board,X,Y,player):
 
     display(board)
 
@@ -18,15 +19,14 @@ def reversible_rock(board,X,Y,player):
     reverse_p = []
     for i in range(-1,2):
         for j in range(-1,2):
-            print("case" ,i, j)
             #判定した内容が相手か判定
             if board[X + i][Y + j] == -player:
                 #相手だったらその先に自分の色の石があるか判定
 
                 tmp = []
                 for g in range(1,8):
-                    if 0 < X + i * g < 8 and 0 < Y + j * g < 8:
-                        if board[X + i * g][Y + j * g] == player:
+                    if 0 < X + (i * g) < 8 and 0 < Y + (j * g) < 8:
+                        if board[X + (i * g)][Y + (j * g)] == player:
                             reverse_p += tmp
                             break
                         tmp.append([X + i * g , Y + j * g ])
@@ -36,61 +36,97 @@ def reversible_rock(board,X,Y,player):
     return reverse_p
 
 
+def init_board():
+    board = []
+    for _ in range(8):
+        board.append([0]*8)
+
+    board[3][3] = 1 #白
+    board[4][4] = 1 #白
+    board[3][4] = -1 #黒
+    board[4][3] = -1 #黒
+    
+    display(board)
+    return board
+
+
+def coin_tos():
+    if (random.randint(0,1) == 1):
+        player = 1
+    else:
+        player = -1
+    return player
+
+def change_player(player):
+     player = -player
+     return player
+
+def revers_rocks(reversible_rocks,board):
+    for i in reversible_rocks:
+        board[i[0]][i[1]] *= -1
+    return board
+
+def count_0s_on_board(board):
+    count0 = 0
+    for i in board:
+        for j in i:
+            if (j == 0):
+                count0 += 1
+    return count0
+
+def end_game(board):
+    one_count = 0
+    minus_one_count = 0
+    for i in board:
+        for j in i:
+            if (j == 1):
+                one_count += 1
+            elif (j == -1):
+                minus_one_count += 1
+    
+    print(f"one_count:{one_count}")
+    print(f"minus_one_count:{minus_one_count}")
+
+
 
 def main():
-    board = []
-    for _ in range(8):
-        board.append([0]*8)
+    
+    board = init_board()
 
-    board[3][3] = 1 #白
-    board[4][4] = 1 #白
-    board[3][4] = -1 #黒
-    board[4][3] = -1 #黒
+    # player = coin_tos()
+    player = -1
 
-    player = 1 # 白1 黒-1
-    game = True # 1ゲーム中　0ゲーム終了
-
-    while game == True:
-
-        display(board)
-        rock_position = input_rock(player)
-        X =rock_position[0]
-        Y =rock_position[1]
-        # rock_set_board = set_rock(board,X,Y,player)
-        # print(rock_set_board)
-
-def test():
-    board = []
-    for _ in range(8):
-        board.append([0]*8)
-
-    board[3][3] = 1 #白
-    board[4][4] = 1 #白
-    board[3][4] = -1 #黒
-    board[4][3] = -1 #黒
-
-
-    display(board)
-
-    player = 1
     while True:
-        rock = input_rock(player)
-        X = rock[0]
-        Y = rock[1]
+        X, Y = input_rock(player)
 
-        revece_p = reversible_rock(board, X, Y, player)
+        #裏返せる石の数
+        reversible_rocks = count_reversible_rock(board, X, Y, player)
 
-        if len(revece_p) > 0:
+        #裏返せる数がゼロより多かったら処理を続ける
+        if len(reversible_rocks) > 0:
+            #置いた場所を反転
             board[X][Y] = player
 
-            for p in revece_p:
-                board[p[0]][p[1]] *= -1
-
+            #盤面を反転
+            board = revers_rocks(reversible_rocks,board)
+            
+            #プレイヤー交代
+            player = change_player(player)
+        elif len(reversible_rocks) == 0:
+            print("そこには置けません。設置可能な場所を指定してください。")
+        
         display(board)
 
-        player = -player
+        amount_of_0s_on_board = count_0s_on_board(board)
+        
+        print(f"amount_of_0s_on_board : {amount_of_0s_on_board}")
+
+        if (amount_of_0s_on_board == 0):
+            end_game()
+
+        
 
 
 if __name__ == '__main__':
-    # main()
-    test()
+
+    main()
--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-
-
-def main():
-    test_list = []
-    for _ in range(3):
-        test_list.append([0]*3)
-
-    
-    print(*test_list,sep='\n')
-    test_list[0][0] = -2
-    print(*test_list,sep='\n')
-
-    for i in range(-1,2):
-        for j in range(-1,2):
-            test_list[X-i][Y-j] == 1 or -1
-
-    print(*test_list,sep='\n')
-
-if __name__ == '__main__':
-    main()
+import random
+random_num = random.randint(0,1)
+print(random_num)
